32c786f8dc540017e004c42f8b3ae0b1adaac62a
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-03-04 15:07:03 -0600

    saving changes

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,39 @@
 var timerEl = document.getElementById('countdown');
 
+// ceate function for timer
 function countdown() {
-    var timeLeft = 5;
-}
+    var timeLeft = 
+
+function generateQuiz(questions, quizContainer, results, submit)
+
+//questions and answers 
+// 1. An event that is fired when an element has lost focus:
+    // A. focus
+    // B. blur   **
+    // C. focus in
+    // D. focus out
+
+// 2. What symbol is used to break the script in JS to pseudocode?
+    // A. /*
+    // B. //    **
+    // C <-->
+    // D. {}
+
+// 3. What's true of undefined values and undefined variables in JavaScript?
+    // A. undefined is an example of a keyword
+    // B. they are variables with a value
+    // C. if an undefined variable is read, an undefined value is returned   **
+    // D. if an undefined variable is read, a defined value is returned
+
+// 4. What are global variables?
+    // A. variables avaiable throughout the code with no scope   **
+    // B. class names
+    // C. another name for keyword 'this'
+    // D. a type of event listener
+
+// 5. An event that is fired when a pointing device (e.g.,a mouse) 
+// is pressed and released on a single element? 
+    // A. context menu
+    // B. click
+    // C. onclick
+    // D. both B and C   **
