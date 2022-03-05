66ba4d7701e5bcb9d525fb7be4e9400f0bf2015b
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-03-05 12:11:32 -0600

    fix code

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 var timerEl = document.getElementById('countdown');
 var beginButtonEl = document.getElementById('generate');
-var containerEl = document.querySelector('container');
+var containerEl = document.querySelector('.container');
 var counterEl = document.getElementById('counter');
+console.log(containerEl);
 
-
-var counter = 60;
+var counter = 120;
 var correctAnswers = 0;
 var wrongAnswers = 0;
 
@@ -13,46 +13,46 @@ var quizCount = 0;
 //setinterval, settimeout
 var view = 'begin'; // quiz, results, begin
 
-
-
 function beginPage() {
+    console.log(beginPageTemplate);
     return beginPageTemplate;
 
 }
 //Step 1
-beginButtonEl.onlick = function () {
+beginButtonEl.onclick = function () {
     containerEl.innerHTML = beginPage();
     quizPage();
     startTimer();
+    console.log("text");
 }
 
 //Step 2
 function quizPage() {
     containerEl.innerHTML = generateQuiz();
-    generateQuiz();
+    
 }
 
 
 // Step 3
 function startTimer() {
     var counterTimer = setInterval(() => {
-        counter -= 1;
+        --counter;
         // update the counter to page
         counterEl.innerHTML = counter;
         // stop counter if it reaches 0
         if (counter === 0) {
             // is it finished ?
             clearInterval(counterTimer);
-            container.innerHTML = resultPage();
+            containerEl.innerHTML = resultPage();
         }
         console.log("Counter:", counter);
-
+        
     }, 1000);
 }
 //Step 4
 function resultPage() {
-    containerEl.innerHTML = `
- <h1>Reult Page</h1>
+    return `
+ <h1>Result Page</h1>
  <p>Correct Answer: ${correctAnswers}</p>
  <p>Wrong Answer: ${wrongAnswers}</p>
         `;
@@ -62,50 +62,50 @@ const quizzes = [
     {
         title: "1. An event that is fired when an element has lost focus: ",
         choices: [
-            "A) focus",
-            "B) blur",
-            "C) focus in",
-            "D) focus out"
+            "focus",
+            "blur",
+            "focus in",
+            "focus out"
         ],
         correctChoice: 0
     },
     {
         title: "2. What's true of undefined values and undefines variables in JavaScript?",
         choices: [
-            "A) undefined is an example of a keyword",
-            "B) they are variables with a value",
-            "C) if an undefined variable is read, an undefined value is returned",
-            "D) if an undefined variable is read, a defined value is returned"
+            "undefined is an example of a keyword",
+            "they are variables with a value",
+            "if an undefined variable is read, an undefined value is returned",
+            "if an undefined variable is read, a defined value is returned"
         ],
         correctChoice: 0
     },
     {
         title: "3. What symbol is used to break the script in JS to pseudocode?",
         choices: [
-            "A) /*",
-            "B) //",
-            "C) <-->",
-            "D) {}"
+            "/*",
+            "//",
+            "<-->",
+            "{}"
         ],
         correctChoice: 0
     },
     {
         title: "4. An event that is fired when a pointing device (e.g.,a mouse) is pressed and released on a single element?",
         choices: [
-            "A) context menu",
-            "B) click",
-            "C) onclick",
-            "D) both B and C"
+            "context menu",
+            "click",
+            "onclick",
+            "both B and C"
         ],
         correctChoice: 0
     },
     {
         title: "5. what are global variables?",
         choices: [
-            "A) context menu",
-            "B) click",
-            "C) onclick",
-            "D) both B and C"
+            "variables avaiible through the code with no scope",
+            "another name for keyword 'this'",
+            "class names",
+            "types of event listeners"
         ],
         correctChoice: 0
     }
