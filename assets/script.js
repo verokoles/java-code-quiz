ddeb5cee2d6e27657c1f187eca62dae50ba2b61d
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-03-11 17:27:33 -0600

    jaca counter and high scores

--- assets/script.js
+++ assets/script.js
@@ -5,17 +5,51 @@ var counterEl = document.getElementById('counter');
 console.log(containerEl);
 //create submit button variable
 var submitButtonEl = document.getElementById('submit');
+var quizButtonEl = document.querySelector('.quiz-button');
 
 // restart quiz button variable
 var restartButtonEl = document.getElementById('restart');
+//next page button
+// var nextButtonEl = document.getElementById('next'); 
 
 //create counter variable
 var counter = 120;
 var correctAnswers = 0;
 var wrongAnswers = 0;
+var scores = [
+    
+];
+
+if (!scores?.length) {
+    scores =JSON.parse(localStorage.getItem("scores"));
+    console.log("Scores: ", scores);
+}
+
+var counterTimer;
+
+
+
+submitButtonEl.onclick = highestScorePage;
+
+
+var currentQuiz = {};
+
 
 var quizCount = 0;
 
+
+function resetQuiz() {
+
+    counter = 120;
+    correctAnswers = 0;
+    wrongAnswers = 0;
+    scores = [];
+    currentQuiz = {};
+    quizCount = 0;
+    clearInterval(counterTimer)
+    
+
+}
 
 function beginPage() {
     console.log(beginPageTemplate);
@@ -23,44 +57,71 @@ function beginPage() {
 
 }
 //Step 1
-beginButtonEl.onclick = function () {
-    containerEl.innerHTML = beginPage();
-    quizPage();
-    startTimer();
-    console.log("text");
-}
+beginButtonEl.onclick = startQuiz;
 
 //Step 2
 function quizPage() {
     containerEl.innerHTML = generateQuiz();
     
 }
-submitButtonEl.onclick = function () {
-    containerEl.innerHTML = submitPage();
-      resultPage();
 
-}
+
+
+// // next page button to go next 
+// nextButtonEl.onclick = function () {
+//     containerEl.innerHTML = generateQuiz();
+//     quizPage();
+// }
 //submit the quiz
-function submitPage() {
+function resultPage() {
+
+    clearInterval(counterTimer);
     console.log(submitPageTemplate);
-    return `
+    var page =  `
     <h1>Result Page</h1>
+    <p>Username: ${username}</p>
     <p>Correct Answer: ${correctAnswers}</p>
     <p>Wrong Answer: ${wrongAnswers}</p>
     <h2>High Scores</h2>
- <p>Log your initials to save high score!</p>
-           `;
+    <p>Log your initials to save restartQuizhigh score!</p>
+    <button id="score">See highest score</button>
+    <button id="restartQuiz">Restart Quiz</button>
+
+
+ `;
+
+    containerEl.innerHTML = page;
+   }
+
+function highestScorePage() {
+
+    var page = document.createElement("div");
+    var orderListEl = document.createElement("ol");
+    for (var i = 0; i < scores?.length; i++) {
+       var scoreEl = document.createElement("li");
+       scoreEl.textContent = "Username: " + scores[i].username + "  score: " + scores[i].score;
+        page.appendChild(scoreEl)        
+    }
+
+    var buttonEl = document.createElement("button")
+        buttonEl.setAttribute("id", "restartQuizOnResultPage")
+        buttonEl.textContent = "Restart Quiz";
+
+    console.log("Page: ", page);
+
+    page.appendChild(orderListEl)
+    page.appendChild(buttonEl)
+
+    containerEl.innerHTML = "";
+
+    containerEl.appendChild(page)
    }
 
 
-submitButtonEl.onclick = function() {
-    containerEl.innerHTML = submitPage();
-      resultPage();
-}
 //setinterval, settimeout
 // Step 3
 function startTimer() {
-    var counterTimer = setInterval(() => {
+    counterTimer = setInterval(() => {
         --counter;
         // update the counter to page
         counterEl.innerHTML = counter;
@@ -74,33 +135,16 @@ function startTimer() {
         
     }, 1000);
 }
-//Step 4
-function resultPage() {
-    return `
- <h1>Result Page</h1>
- <p>Correct Answer: ${correctAnswers}</p>
- <p>Wrong Answer: ${wrongAnswers}</p>
- <h2>High Scores</h2>
- <p>Log your initials to save high score!</p>
-
- //initials
-// const initialForm = document.getElementById("initials");
-// const scoreButton = document.getElementById("set-score");
-
-// scoreButton.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     const initial = initialForm.initial.value;
-
-//     if (initial === "ABCDEFGHIJKLNMOPQRSTUVWXYZ" || "abcdefghijklmnopqrstuvwxyz") {
-//         alert("Your high score has been logged!");
-//         location.reload();
-//     }
-        `;
+function generateQuestion() {
+ // const quizzes(0);
 }
-//restart quiz
-function restartQuiz() {
-    console.log(restartQuizTemplate);
-    return restartQuizTemplate;
+
+//Start quiz
+function startQuiz() {
+    containerEl.innerHTML = beginPage();
+    resetQuiz();
+    quizPage();
+    startTimer();
 }
 
 
@@ -166,6 +210,8 @@ const quizzes = [
     }
 ];
 
+// choices[3]
+
 // when a user clicks on any of questions 
 //check to see if the answer is correct or not
 // if its wrong, take away 10 seconds
@@ -173,16 +219,92 @@ const quizzes = [
 
 function generateQuiz(questions, quizContainer, results, submit) {
 
-    const currentQuiz = quizzes[quizCount];
+    if (quizzes.length > quizCount) {
+        currentQuiz = quizzes[quizCount]; // 0, 1, 2, 3, 4
+        var choices = currentQuiz.choices;
+    
+    
+    
+        var quiz = `
+              <h2>${currentQuiz.title}</h2>
+            <button data-value="${choices[0]}" class="quiz-button">A) ${choices[0]}
+            <button data-value="${choices[1]}" class="quiz-button">B) ${choices[1]}  
+            <button data-value="${choices[2]}" class="quiz-button">C) ${choices[2]} 
+            <button data-value="${choices[3]}" class="quiz-button">D) ${choices[3]} 
+        `;
+    
+        // TODO - attach the event directly through the node
+    
+        return quiz;
+    }
 
-    const quiz = `
-          <h2>${currentQuiz.title}</h2>
-        <p>A) ${currentQuiz.choices[0]}</p>
-        <p>B) ${currentQuiz.choices[1]}</p>  
-        <p>C) ${currentQuiz.choices[2]}</p> 
-        <p>D) ${currentQuiz.choices[3]}</p> 
-    `;
-
-    return quiz;
+  
 
 }
+
+
+console.log("Quiz Button: ", quizButtonEl);
+
+
+function nextQuestion () {
+    quizCount++;
+    quizPage();
+}
+
+document.onclick = function(event) {
+    var targetEl = event.target;
+
+    if (targetEl.getAttribute("class") === "quiz-button") {
+       handleQuiz(targetEl)
+    }
+
+    if (targetEl.getAttribute("id") === "restartQuiz" || targetEl.getAttribute("id") === "restartQuizOnResultPage") {
+        startQuiz()
+     }
+
+    if (targetEl.getAttribute("id") === "score") {
+        highestScorePage();
+    }
+};
+
+
+function handleQuiz(targetEl) {
+    var userSelection =  targetEl.getAttribute("data-value");
+    var answer = currentQuiz.correctChoice; // 0
+    var choices = currentQuiz.choices;
+
+    var correctAnswer = choices[answer];
+
+    console.log("Quiz: ", currentQuiz);
+    // We're checking for correct/wrong answr
+    if (userSelection ===correctAnswer ) {
+        // alert("Yayyy");
+       
+        correctAnswers++;
+        nextQuestion()
+    } else {
+        wrongAnswers++;
+        // alert("Nooo!!!");
+        nextQuestion()
+    }
+
+
+    if (quizCount <= 4 ) {
+        // TODO - what you want to handl ehere
+        
+       
+    } else {
+        // TODO - Save user to local storage
+        username = prompt("what's your username")
+
+        scores.push({
+            username: username,
+            score: correctAnswers
+        });
+        localStorage.setItem("scores", JSON.stringify(scores))
+        resultPage();
+    }
+}
+
+
+
